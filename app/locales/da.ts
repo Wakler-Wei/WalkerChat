959702cc69431452b661425ec759346fd3b9dd2c
Wakler-Wei W <319230300@qq.com>
2025-02-19 08:07:38 +0000

    fix20250219

--- app/locales/da.ts
+++ app/locales/da.ts
@@ -25,7 +25,7 @@ const da: PartialLocaleType = {
     Input: "Adgangskode",
     Confirm: "OK",
     Later: "Senere",
-    SaasTips: "Hvis det er for svært, kan du starte nu",
+    // SaasTips: "Hvis det er for svært, kan du starte nu",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} beskeder`,
